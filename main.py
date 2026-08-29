c6d5d58d101c87e070eefb5640d45bbc8248f4b5
ApekshaGowda28 <apeksha282007@gmail.com>
2026-08-29 17:18:28 +0530

    updated

--- main.py
+++ main.py
@@ -1,38 +1,77 @@
 import csv
 import os
-name=input("enter student full name:")
-subjects=["python", "Java", "C++", "Databases" ]
-marks=[]
+
+name = input("Enter student full name: ")
+
+subjects = ["Python", "Java", "C++", "Databases"]
+
+marks = []
+
 for subject in subjects:
-    mark=float(input(f"enter marks for {subject}: "))
+    mark = float(input(f"Enter marks for {subject}: "))
     marks.append(mark)
-total=sum(marks)
-average=total/len(marks)
-if average>=90:
-    grade="A+"
-elif average>=80:
-    grade="A"
-elif average>=70:
-    grade="B"
-elif average>=60:
-    grade="B+"
-elif average>=50:
-    grade="D"
+
+total = sum(marks)
+average = total / len(marks)
+
+# Grade
+if average >= 90:
+    grade = "A+"
+    grade_point = 10
+elif average >= 80:
+    grade = "A"
+    grade_point = 9
+elif average >= 70:
+    grade = "B"
+    grade_point = 8
+elif average >= 60:
+    grade = "B+"
+    grade_point = 7
+elif average >= 50:
+    grade = "D"
+    grade_point = 6
 else:
-    grade="F"
-results="PASS" if average>=40 else "FAIL"
-print(f"Student Name: {name}")
+    grade = "F"
+    grade_point = 0
+
+# CGPA
+cgpa = grade_point
+
+# Result
+results = "PASS" if average >= 40 else "FAIL"
+
+print(f"\nStudent Name: {name}")
 print(f"Total Marks: {total}")
 print(f"Average Marks: {average:.2f}")
 print(f"Grade: {grade}")
+print(f"CGPA: {cgpa:.2f}")
 print(f"Result: {results}")
-file_name="student_results.csv"
-file_exists=os.path.exists(file_name)
-with open(file_name,mode="a",newline="")as file:
-    writer=csv.writer(file)
+
+# Save results to CSV
+file_name = "student_results.csv"
+
+file_exists = os.path.exists(file_name)
+
+with open(file_name, mode="a", newline="") as file:
+    writer = csv.writer(file)
+
     if not file_exists:
-        writer.writerow(["Name","total Marks","average Marks","Grade","Result"])
-    writer.writerow([name,total,round(average,2),grade,results])
-print("\n Student results saved to student_results.csv")
-            
-        
+        writer.writerow([
+            "Name",
+            "Total Marks",
+            "Average Marks",
+            "Grade",
+            "CGPA",
+            "Result"
+        ])
+
+    writer.writerow([
+        name,
+        total,
+        round(average, 2),
+        grade,
+        cgpa,
+        results
+    ])
+
+print("\nStudent results saved to student_results.csv")
